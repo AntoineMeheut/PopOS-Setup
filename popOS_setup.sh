1b2d75356a495d865c5233ad9ee3b27a66e11fe5
Antoine.Meheut <34522927+AntoineMeheut@users.noreply.github.com>
2022-03-23 17:10:59 +0100

    Update popOS_setup.sh

--- popOS_setup.sh
+++ popOS_setup.sh
@@ -150,12 +150,12 @@ if [ "$load_tmp_file" = "no" ]; then
 		if [ -n "$(awk '$0 ~ /^\#/' $MEMFILE)" ]; then
 			read -rp "$(printf "Voulez-vous installer \e[01;33m%s\e[00m ? (%s) (Y/n) > " "$(awk '{print $1}' $MEMFILE | tr '_' ' ')" "$(cut -d' ' -f2- $MEMFILE)")"
 			[ "${REPLY,,}" == 'y' -o -z "$REPLY" ] && SKIP_CATEGORY=no || SKIP_CATEGORY=yes
-		fi
-		# Traite les applications en catégories
-		if [ -n "$(awk '$0 ~ /\t/' $MEMFILE)" -a "$SKIP_CATEGORY" = 'non' ]; then
-			read -rp "$(printf "Logiciels à installer : \e[33m%s\e[00m (Y/n) " "$(awk '{print $1}' $MEMFILE | tr '_' ' ')")"
-			[ "${REPLY,,}" == 'y' -o -z "$REPLY" ] && \
+			# Traite les applications en catégories
+			if [ -n "$(awk '$0 ~ /\t/' $MEMFILE)" -a "$SKIP_CATEGORY" = 'non' ]; then
+				read -rp "$(printf "Logiciels à installer : \e[33m%s\e[00m (Y/n) " "$(awk '{print $1}' $MEMFILE | tr '_' ' ')")"
+				[ "${REPLY,,}" == 'y' -o -z "$REPLY" ] && \
 				TO_DNF+=($(cut -d' ' -f2- $MEMFILE))
+			fi
 		fi
 
 		chmod u+w $MEMFILE
